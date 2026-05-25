23a020613474b3df83c682fbdfc1f3c719cfe2a1
Minseok Kim <43489974+Mstone8370@users.noreply.github.com>
2026-05-25 23:52:39 +0900

    MathJax 로드하는 스크립트 추가

---
+++ Script/LoadMathJax.js
@@ -0,0 +1,21 @@
+(function() {
+  if (window.MathJax) return;
+
+  window.MathJax = {
+    tex: {
+      inlineMath: [['$', '$'], ['\\(', '\\)']],
+      displayMath: [['$$', '$$'], ['\\[', '\\]']],
+      processEscapes: true
+    },
+    options: {
+      skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
+    }
+  };
+
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
+
+// <script src="https://mstone8370.github.io/blog-widgets/load-mathjax.js"></script>
